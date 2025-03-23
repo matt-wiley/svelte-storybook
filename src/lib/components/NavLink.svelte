c6d72cdc9855867c7824a4b81ae2d71e9695347c
Matt Wiley <mattwiley+github@fastmail.com>
2025-03-22 20:48:30 -0400

    added: more components

---
+++ src/lib/components/NavLink.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let label: string = 'NavLink';
+	export let size: 'small' | 'medium' | 'large' = 'medium';
+	export let href: string = '#';
+
+	const configurableStyles = {
+		small: 'p-2',
+		medium: 'p-3',
+		large: 'p-4'
+	};
+
+	$: sizeStyle= configurableStyles[size]; 
+  $: configuredClasses = [sizeStyle].join(' ').trim();
+
+</script>
+
+<a
+	class={`block font-bold cursor-pointer hover:text-red-500 ${configuredClasses}`}
+	{href}>{label}</a
+>
--- src/stories/NavLink.stories.svelte
+++ src/stories/NavLink.stories.svelte
@@ -1,30 +1,26 @@
 <script module>
   import { defineMeta } from '@storybook/addon-svelte-csf';
-  import Button from '$lib/components/Button.svelte';
-  import { fn } from '@storybook/test';
+  import NavLink from '$lib/components/NavLink.svelte';
 
   // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
   const { Story } = defineMeta({
-    title: 'Example/Button',
-    component: Button,
+    title: 'Example/NavLink',
+    component: NavLink,
     tags: ['autodocs'],
     argTypes: {
       size: {
         control: { type: 'select' },
         options: ['small', 'medium', 'large'],
       },
+      href: { control: 'text' }
     },
-    args: {
-      onClick: fn(),
-    }
+    args: {}
   });
 </script>
 
 <!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
-<Story name="Primary" args={{ primary: true, label: 'Button' }} />
+<Story name="Small" args={{ size: "small" }} />
 
-<Story name="Secondary" args={{ label: 'Button' }} />
+<Story name="Medium" args={{ size: "medium" }} />
 
-<Story name="Large" args={{ size: 'large', label: 'Button' }} />
-
-<Story name="Small" args={{ size: 'small', label: 'Button' }} />
+<Story name="Large" args={{ size: "large" }} />
