f46dd29fbb7f9edc303d96b9e3665233f375a59d
Matt Wiley <mattwiley+github@fastmail.com>
2025-03-23 20:40:45 -0400

    updated: removed List stories, added stories for TwoLineListItem

---
+++ src/stories/TwoLineListItem.stories.svelte
@@ -0,0 +1,26 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import TwoLineListItem from '$lib/components/TwoLineListItem.svelte';
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    title: 'Components/TwoLineListItem',
+    component: TwoLineListItem,
+    tags: ['autodocs'],
+    argTypes: {
+      title: { control: 'text' },
+      subtitle: { control: 'text' },
+      imageUrl: { control: 'text' },
+      showPlaceholder: { control: 'boolean' },
+      placeholderImage: { control: 'text' }
+    },
+    args: {
+      title: 'Alice',
+      subtitle: 'Developer',
+      showPlaceholder: false
+    }
+  });
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Default" />
